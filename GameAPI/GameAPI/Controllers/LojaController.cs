207f8bb5114f075a170ad517eb3057cbae71b787
be3013 <be3012@hotmail.com>
2021-11-09 18:02:52 -0300

    Layout da view de lojas
    Estrutura inicial da view de lojas e jogos. Falta preencher os controllers para os botões dentro da view funcionarem.

---
+++ GameAPI/GameAPI/Controllers/LojaController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameAPI.Controllers
+{
+    public class LojaController : Controller
+    {
+        public IActionResult Index(String Nome)
+        {
+            if (!string.IsNullOrEmpty(Nome))
+            {
+
+            }
+
+            return View();
+        }
+    }
+}
