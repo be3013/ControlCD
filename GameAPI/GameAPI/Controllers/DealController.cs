5db61df22d042df16fc73f260bde4dd3c65925ec
jotalima72 <48960188+jotalima72@users.noreply.github.com>
2021-11-12 22:54:38 -0300

    view promoções almost complete
    termina ai
    
    Co-Authored-By: Danielbb21 <50447725+Danielbb21@users.noreply.github.com>

---
+++ GameAPI/GameAPI/Controllers/DealController.cs
@@ -0,0 +1,37 @@
+using GameAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameAPI.Controllers
+{
+    public class DealController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public DealController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
