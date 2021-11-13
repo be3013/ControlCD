3f20d102c48d77637e1a658d08a41a88cb9aa90c
Patrick Guilherme da Silva <patrick.guilhermedasilva@gmail.com>
2021-11-12 23:05:46 -0300

    Model de promoção e packages

---
+++ GameAPI/GameAPI/Models/Promocao.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Threading.Tasks;
+
+namespace GameAPI.Models
+{
+    public class Promocao
+    {
+        public int ID { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public double ValorOriginal { get; set; }
+        public double ValorDesconto { get; set; }
+        public Blob ImagemCapa { get; set; }
+    }
+}
